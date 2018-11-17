9bb3f0bf0288770f40d3ce0ffaf7362b1d1af4f5
Andre Kohn <andre@kohn.io>
2018-11-17 13:02:02 +0100

    Work on team encounters

--- functions.sql
+++ functions.sql
@@ -106,6 +106,84 @@ RETURNS TABLE(
     END
 $$ LANGUAGE plpgsql;
 
+-- Bulk load team encounter.
+-- This will attempt to insert the encounters of all existing matches.
+-- (~ 2 hours)
+CREATE OR REPLACE FUNCTION didact_bulkload_team_encounters()
+RETURNS VOID AS $$
+    DECLARE
+        t TIMESTAMP := clock_timestamp();
+    BEGIN
+        RAISE NOTICE 'Bulk loading team encounters';
+
+        WITH x_ AS (
+            SELECT
+                p_id AS player_id,
+                mp_match_id AS match_id,
+                mp_player_idx AS player_idx,
+                mp_team_id AS team_id,
+                rank() over (partition by mp_match_id, mp_team_id order by p_id asc) as rank
+            FROM match_player, player
+            WHERE mp_gamertag = p_gamertag
+        ), t_ AS (
+            SELECT
+                m1.match_id AS match_id,
+                m1.team_id AS team_id,
+                m1.player_id AS p1_id,
+                COALESCE(m2.player_id, 0) AS p2_id,
+                COALESCE(m3.player_id, 0) AS p3_id
+            FROM x_ m1
+                LEFT OUTER JOIN x_ m2
+                    ON m1.match_id = m2.match_id
+                    AND m1.team_id = m2.team_id
+                    AND m2.rank = 2
+                LEFT OUTER JOIN x_ m3
+                    ON m1.match_id = m3.match_id
+                    AND m1.team_id = m3.team_id
+                    AND m3.rank = 3
+            WHERE m1.rank = 1
+        )
+        INSERT INTO team_encounter(
+                te_match_id,
+                te_t1_p1_id,
+                te_t1_p2_id,
+                te_t1_p3_id,
+                te_t2_p1_id,
+                te_t2_p2_id,
+                te_t2_p3_id,
+                te_start_date,
+                te_duration,
+                te_match_outcome,
+                te_map_uuid,
+                te_match_uuid,
+                te_playlist_uuid,
+                te_season_uuid
+        )
+        SELECT
+                m.m_id,
+                t1.p1_id, t1.p2_id, t1.p3_id,
+                t2.p1_id, t2.p2_id, t2.p3_id,
+                m.m_start_date,
+                m.m_duration,
+                mt.mt_match_outcome,
+                m.m_map_uuid,
+                m.m_match_uuid,
+                m.m_playlist_uuid,
+                m.m_season_uuid
+        FROM t_ t1, t_ t2, match m, match_team mt
+        WHERE t1.match_id = t2.match_id
+        -- The < predicate prevents the nested loop join (estimates are far off here)
+        AND t1.team_id < t2.team_id
+        AND t1.match_id = m.m_id
+        AND mt.mt_match_id = m.m_id
+        AND mt.mt_team_id = 1
+        ON CONFLICT DO NOTHING;
+
+        RAISE NOTICE 'Duration=%', clock_timestamp() - t;
+        END
+$$ LANGUAGE plpgsql;
+
+-- Add missing team encounters.
 CREATE OR REPLACE FUNCTION didact_sync_team_encounters()
 RETURNS VOID AS $$
     DECLARE
@@ -139,14 +217,13 @@ RETURNS VOID AS $$
                     AND m1.team_id = m3.team_id
                     AND m3.rank = 3
             WHERE m1.rank = 1
+        ), tdiff_ AS (
+            SELECT t.*
+            FROM t_ t
+                LEFT OUTER JOIN team_encounter te
+                ON te.te_match_id = t.match_id
+            WHERE te.te_t1_p1_id IS NULL
         )
---        , tdiff_ AS (
---            SELECT t.*
---            FROM t_ t
---                LEFT OUTER JOIN team_encounter te
---                ON te.te_match_id = t.match_id
---            WHERE te.te_t1_p1_id IS NULL
---        )
         INSERT INTO team_encounter(
                 te_match_id,
                 te_t1_p1_id,
@@ -174,7 +251,7 @@ RETURNS VOID AS $$
                 m.m_match_uuid,
                 m.m_playlist_uuid,
                 m.m_season_uuid
-        FROM t_ t1, t_ t2, match m, match_team mt
+        FROM tdiff_ t1, tdiff_ t2, match m, match_team mt
         WHERE t1.match_id = t2.match_id
         -- The < predicate prevents the nested loop join (estimates are far off here)
         AND t1.team_id < t2.team_id
